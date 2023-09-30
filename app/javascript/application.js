0f81ff033f7c3381f9885081cee0d09cdc4c1166
Reggeditt <tkodjosarso@gmail.com>
2023-09-30 12:32:08 +0000

    update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,14 +5,16 @@ import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Greeting from "./components/greeting";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Router } from "react-router-dom";
 
 
 const App = () => {
   // add react router here
   return (
     <BrowserRouter>
-      <Route path="/" component={Greeting} />
+      <Router>
+        <Route path="/" component={<Greeting />} />
+      </Router>
     </BrowserRouter>
   );
 }
